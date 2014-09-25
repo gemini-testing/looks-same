94bfe9b49e5f5d9d1fc56dfa1e449528d79de81b
Sergey Tatarintsev <sevinf@yandex-team.ru>
2014-09-25 14:12:31 +0400

    Building diff API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 var fs = require('fs'),
     PNG = require('pngjs').PNG,
+    parseColor = require('parse-color'),
     colorDiff = require('color-diff');
 
 function readPNG(path, callback) {
@@ -53,13 +54,11 @@ function readPair(first, second, callback) {
     });
 }
 
-function arePNGsLookSame(png1, png2) {
-    if (png1.width !== png2.width || png1.height !== png2.height) {
-        return false;
-    }
-    var width = png1.width,
-        height = png1.height;
+function forEachPixelPair(png1, png2, callback) {
+    var width = Math.min(png1.width, png2.width),
+        height = Math.min(png1.height, png2.height);
 
+    outer:
     for (var y = 0; y < height; y++) {
         for (var x = 0; x < width; x++) {
             var idx = (width * y + x) * 4,
@@ -73,17 +72,31 @@ function arePNGsLookSame(png1, png2) {
                     G: png2.data[idx + 1],
                     B: png2.data[idx + 2]
                 };
-
-            if (!areColorsLookSame(color1, color2)) {
-                return false;
+            if (callback(color1, color2, idx)) {
+                break outer;
             }
         }
     }
+}
 
-    return true;
+function arePNGsLookSame(png1, png2) {
+    if (png1.width !== png2.width || png1.height !== png2.height) {
+        return false;
+    }
+    var result = true;
+    forEachPixelPair(png1, png2, function(color1, color2) {
+        if (!areColorsLookSame(color1, color2)) {
+            result = false;
+            return true;
+        }
+    });
+    return result;
 }
 
 function areColorsLookSame(c1, c2) {
+    if (areColorsSame(c1, c2)) {
+        return true;
+    }
     /*jshint camelcase:false*/
     var lab1 = colorDiff.rgb_to_lab(c1),
         lab2 = colorDiff.rgb_to_lab(c2);
@@ -91,11 +104,69 @@ function areColorsLookSame(c1, c2) {
     return colorDiff.diff(lab1, lab2) < 2.3;
 }
 
-module.exports = function looksSame(reference, image, callback) {
+function areColorsSame(c1, c2) {
+    return c1.R === c2.R &&
+        c1.G === c2.G,
+        c1.B === c2.B;
+}
+
+module.exports = exports = function looksSame(reference, image, callback) {
     readPair(reference, image, function(error, result) {
         if (error) {
             return callback(error, null);
         }
         callback(null, arePNGsLookSame(result.first, result.second));
+    });
+};
+
+function buildDiffStream(png1, png2, options) {
+    var width = Math.max(png1.width, png2.width),
+        height = Math.max(png1.height, png2.height),
+        highlightColor = options.highlightColor,
+        result = new PNG({
+            width: width,
+            height: height
+        });
+
+    forEachPixelPair(png1, png2, function(color1, color2, idx) {
+        if (!areColorsLookSame(color1, color2)) {
+            result.data[idx] = highlightColor.R;
+            result.data[idx + 1] = highlightColor.G;
+            result.data[idx + 2] = highlightColor.B;
+            result.data[idx + 3] = 255;
+        } else {
+            result.data[idx] = color1.R;
+            result.data[idx + 1] = color1.G;
+            result.data[idx + 2] = color1.B;
+            result.data[idx + 3] = 255;
+        }
+    });
+    return result.pack();
+}
+
+function parseColorString(str) {
+    var parsed = parseColor(str);
+    return {
+        R: parsed.rgb[0],
+        G: parsed.rgb[1],
+        B: parsed.rgb[2]
+    };
+}
+
+exports.saveDiff = function saveDiff(opts, callback) {
+    readPair(opts.reference, opts.current, function(error, result) {
+        var diffOptions = {
+                highlightColor: parseColorString(opts.highlightColor)
+            },
+            stream = buildDiffStream(result.first, result.second, diffOptions),
+            writeStream = fs.createWriteStream(opts.diff);
+        stream.pipe(writeStream);
+
+        writeStream.on('error', function(error) {
+            callback(error);
+        });
+        writeStream.on('finish', function() {
+            callback(null);
+        });
     });
 };
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 'use strict';
 var path = require('path'),
     fs = require('fs'),
+    temp = require('temp'),
     expect = require('chai').expect,
 
     looksSame = require('..');
@@ -61,6 +62,61 @@ describe('looksSame', function() {
             looksSame(getImage('image1.png'), getImage('image-tall.png'), function(error, equal) {
                 expect(error).to.equal(null);
                 expect(equal).to.equal(false);
+                done();
+            });
+        });
+    });
+});
+
+describe('saveDiff', function() {
+    beforeEach(function() {
+        this.tempName = temp.path({suffix: '.png'});
+    });
+
+    afterEach(function() {
+        if (fs.existsSync(this.tempName)) {
+            fs.unlinkSync(this.tempName);
+        }
+    });
+
+    it('should craate an image file a diff for for two images', function(done) {
+        var _this = this;
+        looksSame.saveDiff({
+            reference: imagePath('image1.png'),
+            current: imagePath('image3.png'),
+            diff: this.tempName,
+            highlightColor: '#ff00ff'
+        }, function() {
+            expect(fs.existsSync(_this.tempName)).to.equal(true);
+            done();
+        });
+    });
+
+    it('should create a proper diff', function(done) {
+        var _this = this;
+        looksSame.saveDiff({
+            reference: imagePath('image1.png'),
+            current: imagePath('image3.png'),
+            diff: this.tempName,
+            highlightColor: '#ff00ff'
+        }, function() {
+            looksSame(imagePath('image1-3_diff_magenta.png'), _this.tempName, function(error, equal) {
+                expect(equal).to.equal(true);
+                done();
+            });
+        });
+    });
+
+    it('should allow to change highlight color', function(done) {
+        var _this = this;
+        looksSame.saveDiff({
+            reference: imagePath('image1.png'),
+            current: imagePath('image3.png'),
+            diff: this.tempName,
+            highlightColor: '#00FF00'
+        }, function() {
+            looksSame(imagePath('image1-3_diff_green.png'), _this.tempName, function(error, equal) {
+                expect(equal).to.equal(true);
                 done();
             });
         });
