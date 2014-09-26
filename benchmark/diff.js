6da8cd385bcef335e201becbf85313000fd56c37
Sergey Tatarintsev <sevinf@yandex-team.ru>
2014-09-26 11:30:36 +0400

    Add benchmark for large images

--- benchmark/diff.js
+++ benchmark/diff.js
@@ -12,30 +12,35 @@ function imagePath(image) {
 var refImage = imagePath('image1.png'),
     currImage = imagePath('image3.png');
 
-suite('diff', function() {
-    var path1 = temp.path({suffix: '.png'}),
-        path2 = temp.path({suffix: '.png'});
+function benchamrkDiff(title, refImage, currImage) {
+    suite(title, function() {
+        var path1 = temp.path({suffix: '.png'}),
+            path2 = temp.path({suffix: '.png'});
 
-    set('iterations', 1000);
-    bench('looksSame', function(next) {
-        looksSame.saveDiff({
-            reference: refImage,
-            current: currImage,
-            diff: path1,
-            highlightColor: '#ff00ff'
-        }, next);
-    });
+        set('iterations', 1000);
+        bench('looksSame', function(next) {
+            looksSame.saveDiff({
+                reference: refImage,
+                current: currImage,
+                diff: path1,
+                highlightColor: '#ff00ff'
+            }, next);
+        });
 
-    bench('gm', function(next) {
-        gm.compare(
-            refImage,
-            currImage,
-            {
-                file: path2,
-                highlightStyle: 'assign',
-                highlightColor: '"#ff00ff"'
-            },
-            next
-        );
+        bench('gm', function(next) {
+            gm.compare(
+                refImage,
+                currImage,
+                {
+                    file: path2,
+                    highlightStyle: 'assign',
+                    highlightColor: '"#ff00ff"'
+                },
+                next
+            );
+        });
     });
-});
+}
+
+benchamrkDiff('small diff', imagePath('image1.png'), imagePath('image3.png'));
+benchamrkDiff('large diff', imagePath('large-image1.png'), imagePath('large-image2.png'));
