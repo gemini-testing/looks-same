b33a6f925701a3ed6cfe9479cf3d8ad290320be5
Anton Usmansky <cody0@yandex-team.ru>
2022-09-16 20:08:16 +0300

    feat: async-await interface
    BREAKING CHANGE: dropped old node-style callback interface support

--- index.js
+++ index.js
@@ -48,27 +48,29 @@ const createComparator = (png1, png2, opts) => {
     return comparator;
 };
 
-const iterateRect = (width, height, callback, endCallback) => {
-    const processRow = (y) => {
-        setImmediate(() => {
-            for (let x = 0; x < width; x++) {
-                callback(x, y);
-            }
+const iterateRect = async (width, height, callback) => {
+    return new Promise((resolve) => {
+        const processRow = (y) => {
+            setImmediate(() => {
+                for (let x = 0; x < width; x++) {
+                    callback(x, y);
+                }
 
-            y++;
+                y++;
 
-            if (y < height) {
-                processRow(y);
-            } else {
-                endCallback();
-            }
-        });
-    };
+                if (y < height) {
+                    processRow(y);
+                } else {
+                    resolve();
+                }
+            });
+        };
 
-    processRow(0);
+        processRow(0);
+    });
 };
 
-const buildDiffImage = (png1, png2, options, callback) => {
+const buildDiffImage = async (png1, png2, options) => {
     const width = Math.max(png1.width, png2.width);
     const height = Math.max(png1.height, png2.height);
     const minWidth = Math.min(png1.width, png2.width);
@@ -76,7 +78,7 @@ const buildDiffImage = (png1, png2, options, callback) => {
     const highlightColor = options.highlightColor;
     const result = png.empty(width, height);
 
-    iterateRect(width, height, (x, y) => {
+    await iterateRect(width, height, (x, y) => {
         if (x >= minWidth || y >= minHeight) {
             result.setPixel(x, y, highlightColor);
             return;
@@ -90,7 +92,9 @@ const buildDiffImage = (png1, png2, options, callback) => {
         } else {
             result.setPixel(x, y, color1);
         }
-    }, () => callback(result));
+    });
+
+    return result;
 };
 
 const parseColorString = (str) => {
@@ -137,111 +141,78 @@ const getMaxDiffBounds = (first, second) => {
     };
 };
 
-module.exports = exports = async function looksSame(image1, image2, opts, callback) {
-    if (!callback) {
-        callback = opts;
-        opts = {};
-    }
-
+module.exports = exports = async function looksSame(image1, image2, opts = {}) {
     opts = prepareOpts(opts);
     [image1, image2] = utils.formatImages(image1, image2);
 
-    try {
-        const {first, second} = await utils.readPair(image1, image2, utils.readBufferCb);
-        const areBuffersEqual = utils.areBuffersEqual(first, second);
+    const {first, second} = await utils.readPair(image1, image2, utils.readBufferCb);
+    const areBuffersEqual = utils.areBuffersEqual(first, second);
 
-        const refImg = {size: {width: first.width, height: first.height}};
-        const metaInfo = {refImg};
+    const refImg = {size: {width: first.width, height: first.height}};
+    const metaInfo = {refImg};
 
-        if (areBuffersEqual) {
-            const diffBounds = (new DiffArea()).area;
-            process.nextTick(() => callback(null, {equal: true, metaInfo, diffBounds, diffClusters: [diffBounds]}));
+    if (areBuffersEqual) {
+        const diffBounds = (new DiffArea()).area;
 
-            return;
-        }
-
-        if (first.width !== second.width || first.height !== second.height) {
-            const diffBounds = getMaxDiffBounds(first, second);
-            process.nextTick(() => callback(null, {equal: false, metaInfo, diffBounds, diffClusters: [diffBounds]}));
-
-            return;
-        }
-
-        const {first: png1, second: png2} = await utils.readPair(
-            {...image1, source: first.buffer},
-            {...image2, source: second.buffer},
-            utils.readPngCb
-        );
-
-        const comparator = createComparator(png1, png2, opts);
-        const {stopOnFirstFail, shouldCluster, clustersSize} = opts;
-
-        utils.getDiffPixelsCoords(png1, png2, comparator, {stopOnFirstFail, shouldCluster, clustersSize}, ({diffArea, diffClusters}) => {
-            const diffBounds = diffArea.area;
-            const equal = diffArea.isEmpty();
-
-            callback(null, {equal, metaInfo, diffBounds, diffClusters});
-        });
-    } catch (err) {
-        return callback(err);
+        return {equal: true, metaInfo, diffBounds, diffClusters: [diffBounds]};
     }
+
+    if (first.width !== second.width || first.height !== second.height) {
+        const diffBounds = getMaxDiffBounds(first, second);
+
+        return {equal: false, metaInfo, diffBounds, diffClusters: [diffBounds]};
+    }
+
+    const {first: png1, second: png2} = await utils.readPair(
+        {...image1, source: first.buffer},
+        {...image2, source: second.buffer},
+        utils.readPngCb
+    );
+
+    const comparator = createComparator(png1, png2, opts);
+    const {stopOnFirstFail, shouldCluster, clustersSize} = opts;
+
+    const {diffArea, diffClusters} = await utils.getDiffPixelsCoords(png1, png2, comparator, {stopOnFirstFail, shouldCluster, clustersSize});
+    const diffBounds = diffArea.area;
+    const equal = diffArea.isEmpty();
+
+    return {equal, metaInfo, diffBounds, diffClusters};
 };
 
-exports.getDiffArea = function(image1, image2, opts, callback) {
-    if (!callback) {
-        callback = opts;
-        opts = {};
-    }
-
+exports.getDiffArea = async function(image1, image2, opts = {}) {
     opts = prepareOpts(opts);
     [image1, image2] = utils.formatImages(image1, image2);
 
-    utils
-        .readPair(image1, image2)
-        .then(({first, second}) => {
-            if (first.width !== second.width || first.height !== second.height) {
-                return process.nextTick(() => callback(null, getMaxDiffBounds(first, second)));
-            }
+    const {first, second} = await utils.readPair(image1, image2);
 
-            const comparator = createComparator(first, second, opts);
+    if (first.width !== second.width || first.height !== second.height) {
+        return getMaxDiffBounds(first, second);
+    }
 
-            utils.getDiffPixelsCoords(first, second, comparator, opts, ({diffArea}) => {
-                if (diffArea.isEmpty()) {
-                    return callback(null, null);
-                }
+    const comparator = createComparator(first, second, opts);
 
-                callback(null, diffArea.area);
-            });
-        })
-        .catch(error => {
-            callback(error);
-        });
+    const {diffArea} = await utils.getDiffPixelsCoords(first, second, comparator, opts);
+
+    if (diffArea.isEmpty()) {
+        return null;
+    }
+
+    return diffArea.area;
 };
 
-exports.createDiff = function saveDiff(opts, callback) {
+exports.createDiff = async function saveDiff(opts) {
     opts = prepareOpts(opts);
 
     const [image1, image2] = utils.formatImages(opts.reference, opts.current);
+    const {first, second} = await utils.readPair(image1, image2);
+    const diffImage = await buildDiffImage(first, second, {
+        highlightColor: parseColorString(opts.highlightColor),
+        comparator: createComparator(first, second, opts)
+    });
 
-    utils
-        .readPair(image1, image2)
-        .then(({first, second}) => {
-            const diffOptions = {
-                highlightColor: parseColorString(opts.highlightColor),
-                comparator: createComparator(first, second, opts)
-            };
-
-            buildDiffImage(first, second, diffOptions, (result) => {
-                if (opts.diff === undefined) {
-                    result.createBuffer(callback);
-                } else {
-                    result.save(opts.diff, callback);
-                }
-            });
-        })
-        .catch(error => {
-            callback(error);
-        });
+    return opts.diff === undefined
+        ? diffImage.createBuffer()
+        : diffImage.save(opts.diff);
 };
 
 exports.colors = (color1, color2, opts) => {
